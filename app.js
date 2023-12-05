8696cf1d85210c80428c7101c577f8de7b64cab4
FelipeMrq <felipe,mrq98@gmail.com>
2023-12-05 10:51:03 -0300

    Deixando o Jogo mais dificil

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaDeNumeroSorteados = [];
-let numeroLimite = 10
+let numeroLimite = 100;
 let numeroSecreto  = gerarNumeroAleatorio();
 let tentativa = 1
 
@@ -13,7 +13,7 @@ function exibirTextoNaTela(tag,texto) {
 }
 function mensagemInicial() {
     exibirTextoNaTela ('h1', 'Jogo do numero Secreto');
-    exibirTextoNaTela ('p', 'Escolha um numero de 1 a 10');
+    exibirTextoNaTela ('p', 'Escolha um numero de 1 a 100');
 
 }
 
